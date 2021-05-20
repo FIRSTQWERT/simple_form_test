8d42530de1e75f2ae34a0a74890818e7bf53760e
FIRSTQWERT <firstqwert@gmail.com>
2021-05-20 14:31:45 +0300

    Nothing here12312

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,16 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-//import com.codeborne.selenide.SelenideElement;
-//import com.codeborne.selenide.commands.Click;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.By;
 
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
-//import static com.codeborne.selenide.Selectors.byCssSelector;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
